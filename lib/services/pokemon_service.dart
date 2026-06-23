f888c34eb4f4b37b5fac5ece4a14618ea785f9cb
jainvasu1 <vasubajaj2001@gmail.com>
2026-06-23 12:00:18 +0530

    Fix: using ty and catch

--- lib/services/pokemon_service.dart
+++ lib/services/pokemon_service.dart
@@ -8,41 +8,53 @@ class PokemonService {
   final String baseUrl = 'https://pokeapi.co/api/v2';
 
   Future<List<Pokemon>> fetchPokemon(int offset) async {
-    final response = await http.get(
-      Uri.parse('$baseUrl/pokemon?limit=10&offset=$offset'),
-    );
+    try {
+      final response = await http.get(
+        Uri.parse('$baseUrl/pokemon?limit=10&offset=$offset'),
+      );
 
-    if (response.statusCode != 200) {
-      throw Exception('Failed to load Pokémon');
-    }
-
-    final data = json.decode(response.body) as Map<String, dynamic>;
-    final results = data['results'] as List;
-    final pokemons = <Pokemon>[];
-
-    for (final result in results) {
-      final detailResponse = await http.get(Uri.parse(result['url'] as String));
-
-      if (detailResponse.statusCode != 200) {
-        throw Exception('Failed to load Pokémon details');
+      if (response.statusCode != 200) {
+        throw Exception('Failed to load Pokémon');
       }
 
-      final detailData =
-          json.decode(detailResponse.body) as Map<String, dynamic>;
-      pokemons.add(Pokemon.fromJson(detailData));
-    }
+      final data = json.decode(response.body) as Map<String, dynamic>;
+      final results = data['results'] as List;
+      final pokemons = <Pokemon>[];
 
-    return pokemons;
+      for (final result in results) {
+        final detailResponse =
+            await http.get(Uri.parse(result['url'] as String));
+
+        if (detailResponse.statusCode != 200) {
+          throw Exception('Failed to load Pokémon details');
+        }
+
+        final detailData =
+            json.decode(detailResponse.body) as Map<String, dynamic>;
+
+        pokemons.add(Pokemon.fromJson(detailData));
+      }
+
+      return pokemons;
+    } catch (e) {
+      throw Exception('Error fetching Pokémon: $e');
+    }
   }
 
   Future<Pokemon> fetchPokemonById(int id) async {
-    final response = await http.get(Uri.parse('$baseUrl/pokemon/$id'));
+    try {
+      final response = await http.get(
+        Uri.parse('$baseUrl/pokemon/$id'),
+      );
 
-    if (response.statusCode != 200) {
-      throw Exception('Failed to load Pokémon');
+      if (response.statusCode != 200) {
+        throw Exception('Failed to load Pokémon');
+      }
+
+      final data = json.decode(response.body) as Map<String, dynamic>;
+      return Pokemon.fromJson(data);
+    } catch (e) {
+      throw Exception('Error fetching Pokémon by ID: $e');
     }
-
-    final data = json.decode(response.body) as Map<String, dynamic>;
-    return Pokemon.fromJson(data);
   }
 }
